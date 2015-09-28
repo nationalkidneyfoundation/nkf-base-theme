87ee41d61d0988dd7ccb3d31eed6de14d9ad5fdf
Jacob Lane <lane.jacob@gmail.com>
2015-09-28 15:25:18 -0400

    spelling

--- template.php
+++ template.php
@@ -34,7 +34,7 @@ function nkf_base_preprocess_page(&$vars) {
 
   // donate form js
   $vars['has_image'] = false;
-  if (!empty($vars['node']) && in_array($vars['node']->type, array('donation','membership_page','fundraising-campaign'))) {
+  if (!empty($vars['node']) && in_array($vars['node']->type, array('donation','membership_page','fundraising_campaign'))) {
     drupal_add_js(drupal_get_path('theme', 'nkf_base') . '/js/donate-form.js');
     if (!empty($vars['node']->field_hero_image) ){// && isset($vars['node']->field_hero_image['und'][0]['value'])) {
       $vars['has_image'] = true;
