324385e81ae034fb2c68d5972b78e8540fa50b55
Jacob Lane <lane.jacob@gmail.com>
2015-09-29 10:27:44 -0400

    updating layouts

--- layouts/75x25/75x25.tpl.php
+++ layouts/75x25/75x25.tpl.php
@@ -2,16 +2,16 @@
 
 ?>
 <div class="grid" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="grid-cell width--100">
+  <div class="grid-cell width--100 padding--lg">
     <?php print $content['top']; ?>
   </div>
-  <div class="grid-cell sm--width--75 width--100">
+  <div class="grid-cell sm--width--75 width--100 padding-y--lg sm--padding-right--lg">
     <?php print $content['left']; ?>
   </div>
-  <div class="grid-cell sm--width--25 width--100">
+  <div class="grid-cell sm--width--25 width--100 padding-y--lg sm--padding-left--lg">
     <?php print $content['right']; ?>
   </div>
-  <div class="grid-cell width--100">
+  <div class="grid-cell width--100 padding--lg">
     <?php print $content['bottom']; ?>
   </div>
 </div>
--- template.php
+++ template.php
@@ -36,7 +36,7 @@ function nkf_base_preprocess_page(&$vars) {
   $vars['has_image'] = false;
   if (!empty($vars['node']) && in_array($vars['node']->type, array('donation','membership_page','fundraising_campaign'))) {
     drupal_add_js(drupal_get_path('theme', 'nkf_base') . '/js/donate-form.js');
-    if (!empty($vars['node']->field_hero_image) ){// && isset($vars['node']->field_hero_image['und'][0]['value'])) {
+    if (!empty($vars['node']->field_hero_image) ){ 
       $vars['has_image'] = true;
     }
   }
