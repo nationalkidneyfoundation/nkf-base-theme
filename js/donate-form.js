6c40d2df78f8aa782471c89516077ea48719534e
Jacob Lane <lane.jacob@gmail.com>
2015-10-15 22:44:02 -0400

    donate form

--- js/donate-form.js
+++ js/donate-form.js
@@ -97,7 +97,7 @@
           var amountOptions = $('<div class="amount-options text-align--center"></div>').insertBefore(textDonation);
           $.each([[50,'$50'],[100,'$100'], [250,'$250'], [500, '$500'], [1000, '$1,000'], [0, 'Other']], function(i, v) {
             var buttonOuter = $('<div class="grid-cell width--33 padding--xxs"></div>').appendTo($(amountOptions));
-            var button = $('<a class="button button--orange--light width--100 padding-y--md padding-x--none" data-amount="' + v[0] + '">' + v[1] + '</a>')
+            var button = $('<a class="button button--gray-1 color--black width--100 padding-y--md padding-x--none" data-amount="' + v[0] + '">' + v[1] + '</a>')
               .appendTo(buttonOuter);
             button.click(function(e) {
                 //$('.amount-options .button').removeClass('active');
