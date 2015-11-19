71c28855010dfad657dedf1c1a5a7b990f6dd7c2
Jacob Lane <lane.jacob@gmail.com>
2015-11-19 16:40:04 -0500

    tabls

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -59,8 +59,8 @@
             <?php if ($title): ?><h1 class="font-size--xxxl"><?php print $title ?></h1><?php endif; ?>
             <?php print render($title_suffix); ?>
             <?php if ($primary_local_tasks): ?><ul class="tabs"><?php print render($primary_local_tasks) ?></ul><?php endif; ?>
-            <?php if ($secondary_local_tasks): ?><ul class="links clearfix"><?php print render($secondary_local_tasks) ?></ul><?php endif; ?>
-            <?php if ($action_links): ?><ul class="links clearfix"><?php print render($action_links); ?></ul><?php endif; ?>
+            <?php if ($secondary_local_tasks): ?><ul class="tabs__local links clearfix"><?php print render($secondary_local_tasks) ?></ul><?php endif; ?>
+            <?php if ($action_links): ?><ul class="tabs__action links clearfix"><?php print render($action_links); ?></ul><?php endif; ?>
             <div id="content" class="clearfix"><?php print render($page['content']) ?></div>
           </div>
         </div>
