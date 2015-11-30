6798b8d6601b387d6adc5822e8b6718549b091c7
Jacob Lane <lane.jacob@gmail.com>
2015-11-30 11:04:42 -0500

    updating page template and a few other basic css

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,82 +1,113 @@
 
+<!-- START OF PAGE TEMPLATE -->
 <div class="<?php print $page_classes; ?>">
-  <header class="bg--orange header background-size--cover background-position--center">
-    <div class="container padding-x--sm sm--padding-x--lg padding-y--md sm--padding-y--lg">
+
+  <!-- HEADER -->
+  <header class="header bg--orange">
+    <div class="container padding-y--md md--padding-y--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
       <div class="grid">
         <?php if ($logo): ?>
-        <div class="grid-cell md--width--50 header__brand">
+        <div class="grid-cell md--width--30 header__brand">
           <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"></a>
         </div>
         <?php endif; ?>
         <?php if (isset($page['top_menu'])): ?>
-        <div class="grid-cell md--width--50 header__nav header__nav--secondary">
+        <div class="grid-cell md--width--70 nav nav--secondary">
           <?php print render($page['top_menu']) ?>
         </div>
         <?php endif; ?>
       </div>
     </div>
-    <?php if ($is_front): ?>
-
-    <?php endif; ?>
-    <?php if (!empty($page['navigation'])) : ?>
-    <div class="container padding--x-lg padding-top--xxl center">
-      <nav class="header__nav header__nav--primary">
-        <?php print render($page['navigation']); ?>
-      </nav>
-    </div>
-    <?php endif; ?>
   </header>
+
+  <!-- NAVIGATION REGION -->
+  <?php if (!empty($page['navigation'])) : ?>
+  <nav class="nav--primary bg--orange">
+    <div class="container padding-top--md md--padding-top--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
+      <?php print render($page['navigation']); ?>
+    </div>
+  </nav>
+  <?php endif; ?>
+
+  <!-- MESSAGES -->
   <?php //if (!empty($page['help']) || ($show_messages && !empty($messages))): ?>
   <?php if ($show_messages && !empty($messages)): ?>
   <section class="background-color--gray-1">
-    <div class="container padding-x--sm sm--padding-x--lg padding-y--lg padding-y--lg sm--padding-y--xl">
+    <div class="container padding-y--md md--padding-y--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
       <?php print render($page['help']); ?>
       <?php if ($show_messages && $messages): print $messages; endif; ?>
     </div>
   </section>
   <?php endif; ?>
 
+
+  <!-- HIGHLIGHTED REGION -->
   <?php if (!empty($page['highlighted'])): ?>
-  <section>
-    <div class="container padding-x--sm sm--padding-x--lg padding-y--lg sm--padding-y--lg sm--padding-y--xl">
+  <section class="padding-x--sm sm--padding-x--lg ">
+    <div class="container padding-y--md md--padding-y--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
       <?php print render($page['highlighted']); ?>
     </div>
   </section>
   <?php endif; ?>
 
-  <section class="main position--relative">
+
+  <!-- MAIN -->
+  <main class="main position--relative">
+    <!-- BACKGROUND IMAGE -->
     <?php if($background_image): ?>
-      <div class="hero sm--show" style="background-image: url('<?php print $background_image_uri; ?>')"></div>
-    <?php endif; ?>
-    <section class="main__title">
-      <?php if ($breadcrumb && FALSE): ?>
       <div class="container">
+        <div class="hero sm--show" style="background-image: url('<?php print $background_image_uri; ?>')"></div>
+      </div>
+    <?php endif; ?>
+
+    <!-- BREADCRUMBS -->
+    <?php if ($breadcrumb && FALSE): ?>
+    <section class="main__breadcrumbs">
+      <div class="container padding-y--md md--padding-y--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
         <?php print $breadcrumb; ?>
       </div>
-      <?php endif; ?>
-      <div class="container padding-x--sm sm--padding-x--lg padding-y--lg sm--padding-y--lg md--padding-y--xl">
+    </section>
+    <?php endif; ?>
+
+    <!-- TITLE -->
+    <section class="main__title">
+      <div class="container padding-top--md md--padding-top--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
         <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="font-size--xl sm--font-size--xxl md--font-size--xxl"><?php print $title ?></h1><?php endif; ?>
+        <?php if ($title): ?><h1 class="font-size--xl sm--font-size--xxl md--font-size--xxxl"><?php print $title ?></h1><?php endif; ?>
         <?php print render($title_suffix); ?>
       </div>
-      <?php if ($primary_local_tasks): ?>
-      <div class="container border-bottom margin-bottom--lg">
+    </section>
+
+    <!-- PRIMARY TABS -->
+    <?php if ($primary_local_tasks): ?>
+    <section class="main__tabs">
+      <div class="container padding-top--md md--padding-top--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
         <ul class="tabs grid list-style-type--none padding--none margin--none"><?php print render($primary_local_tasks) ?></ul>
       </div>
-      <?php endif; ?>
-      <?php if ($secondary_local_tasks): ?>
-      <div class="container margin-bottom--lg">
+    </section>
+    <?php endif; ?>
+
+    <!-- SECONDARY TABS -->
+    <?php if ($secondary_local_tasks): ?>
+    <section class="main__tasks">
+      <div class="container padding-top--md md--padding-top--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
         <ul class="tabs__local grid list-style-type--none padding--none margin--none links "><?php print render($secondary_local_tasks) ?></ul>
       </div>
-      <?php endif; ?>
-      <?php if ($action_links): ?>
-      <div class="container  margin-bottom--lg">
+    </section>
+    <?php endif; ?>
+
+    <!-- ACTION LINKS -->
+    <?php if ($action_links): ?>
+    <section class="main__action-links">
+      <div class="container padding-top--md md--padding-top--lg padding-x--sm sm--padding-x--md md--padding-x--lg">
         <ul class="tabs__action links "><?php print render($action_links); ?></ul>
       </div>
-      <?php endif; ?>
     </section>
+    <?php endif; ?>
+
+    <!-- MAIN CONTENT -->
     <section class="main__content">
-      <div class="container position--relative z-index--1 padding-x--sm sm--padding-x--lg padding-bottom--xl">
+      <div class="container padding-top--md md--padding-top--lg padding-bottom--xxxl padding-x--sm sm--padding-x--md md--padding-x--lg position--relative z-index--1">
         <div class="grid">
           <?php if ($page['sidebar_first']): ?>
             <div class="grid-cell width--25"><?php print render($page['sidebar_first']) ?></div>
@@ -90,12 +121,12 @@
         </div>
       </div>
     </section>
+  </main>
 
-  </section>
 
-
-  <footer class="bg--orange padding-y--xxl">
-    <div class="container padding-x--sm sm--padding-x--lg padding-y--lg sm--padding-y--lg sm--padding-y--xxl">
+  <!-- FOOTER -->
+  <footer class="footer bg--gray-1">
+    <div class="container padding-y--xxxl padding-x--sm sm--padding-x--md md--padding-x--lg ">
       <?php if ($page['footer']): ?>
       <?php print render($page['footer']); ?>
       <?php endif; ?>
@@ -111,4 +142,6 @@
       <?php endif; ?>
     </div>
   </footer>
+
+
 </div>
