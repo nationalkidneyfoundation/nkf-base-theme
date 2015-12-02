7cba5de6cfd64a57d911b2e59cb8d79e44204493
Jacob Lane <lane.jacob@gmail.com>
2015-12-01 23:07:20 -0500

    professionals theme

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -125,7 +125,7 @@
 
 
   <!-- FOOTER -->
-  <footer class="footer bg--gray-1 border-top-width--xl border-color--gray-3 border-style--solid">
+  <footer class="footer bg--gray-1 border-top-width--lg border-color--gray-3 border-style--solid">
     <div class="container padding-y--xxxl padding-x--sm sm--padding-x--md md--padding-x--lg ">
       <?php if ($page['footer']): ?>
       <?php print render($page['footer']); ?>
