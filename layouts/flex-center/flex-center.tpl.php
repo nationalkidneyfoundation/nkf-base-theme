4983ca4fc05737ae0786864407ef89875fd500cc
Jacob Lane <lane.jacob@gmail.com>
2017-01-27 09:10:58 -0500

    adding center template

---
+++ layouts/flex-center/flex-center.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+<div class="container">
+  <div class="grid--center" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+    <?php if (!empty($content['main'])):?>
+      <?php print $content['main']; ?>
+    <?php endif; ?>
+  </div>
+</div>
