1cf21581d0cf82fc1285bb32d560b53f35761e08
Jacob Lane <lane.jacob@gmail.com>
2017-03-20 16:10:39 -0400

    adding icon

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -50,6 +50,12 @@
   <meta name="MobileOptimized" content="width">
   <meta name="HandheldFriendly" content="true">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f15e22">
+  <meta name="theme-color" content="#ffffff">
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <script>
