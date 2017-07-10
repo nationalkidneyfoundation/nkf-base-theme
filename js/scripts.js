a30497dcfdfa31bdc931011173613e9badf0bec8
Jacob Lane <lane.jacob@gmail.com>
2017-07-10 09:08:14 -0400

    on to one

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var nkf_base_init = function($) {
   // utility function to toggle classes on click
   // useful for toggling visibility
-  $('[data-toggle=class]').on('click', function (e) {
+  $('[data-toggle=class]').one('click', function (e) {
     var targets = $(this).attr('data-target').split('|');
     var classes = $(this).attr('data-class').split('|');
     $(targets).each(function(i, v){
