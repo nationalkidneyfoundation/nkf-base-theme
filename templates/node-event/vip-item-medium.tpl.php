dd613aab602b96ceae040dcea915f89991917af2
Tracey Berg-Fulton <traceyb@Traceys-MacBook-Pro.local>
2018-02-14 10:33:17 -0500

    Remove dpm script

--- templates/node-event/vip-item-medium.tpl.php
+++ templates/node-event/vip-item-medium.tpl.php
@@ -3,9 +3,9 @@
 <?php endif;?>
 <div class="grid-cell width--100 padding-bottom--sm padding-right--sm clearfix">
   <?php if(isset($row['field_base_image'])): ?>
-    <div class="height--lg padding-bottom--sm padding-right--lg float--left">
+    <div class="padding-bottom--sm padding-right--lg float--left">
       <img
-        src="<?php print nkf_base_image_url($row['field_base_image']['#path'],'large'); ?>"
+        src="<?php print nkf_base_image_url($row['field_base_image']['#path'],'scale', 300, 300); ?>"
         class="display--block"
         title="<?php print $row['field_base_image']['#title']; ?>"
         alt="<?php print $row['field_base_image']['#alt']; ?>"/>
